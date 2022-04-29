1d5cd5d14941483b2bbf4713050db80b4063131d
omkar2120 <naradomkar212000@gmail.com>
2022-04-29 19:24:18 +0530

    done

---
+++ assignment/src/component/Form.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { useFormik , handleChange ,handleSubmit} from 'formik';
+import *  as Yup from 'yup'
+
+
+const Form = () => {
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters")
+    .max(40, "Password must not exceed 40 characters"),
+  })
+   const formik = useFormik({
+     initialValues:{
+       email:"",
+       password:""
+     },
+     validationSchema,
+     onSubmit:(data) => {
+       console.log(JSON.stringify(data,null , 2))
+     },
+   })
+  return (
+    <div>
+      <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          onSubmit={formik.handleSubmit}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            <p className='error'>{formik.errors.email ? formik.errors.email: null}</p>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+            <p className='error'>{formik.errors.password ? formik.errors.password: null}</p>
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onSubmit={formik.handleSubmit}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      
+    </div>
+  )
+}
+
+export default Form
